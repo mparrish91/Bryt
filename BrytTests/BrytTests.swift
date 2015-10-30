e65600cf3d284dac6cca2ede07395a468712840d
malcolm <mparrish91@gmail.com>
2015-10-30 11:37:16 -0700

    Initial Commit

---
+++ BrytTests/BrytTests.swift
@@ -0,0 +1,36 @@
+//
+//  BrytTests.swift
+//  BrytTests
+//
+//  Created by Malcolm Parrish on 10/30/15.
+//  Copyright © 2015 Bryt. All rights reserved.
+//
+
+import XCTest
+@testable import Bryt
+
+class BrytTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
